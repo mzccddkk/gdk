d37b725bf9c0662c9004d42aa6555d9c9180218d
mzccddkk <mzccddkk@gmail.com>
2022-07-06 18:11:35 +0800

    docs: add annotates

--- hmacx/hmac.go
+++ hmacx/hmac.go
@@ -8,18 +8,21 @@ import (
 	"encoding/hex"
 )
 
+// Md5 Calculate the md5 hash value using the HMAC method
 func Md5(str string, key string) string {
 	h := hmac.New(md5.New, []byte(key))
 	h.Write([]byte(str))
 	return hex.EncodeToString(h.Sum(nil))
 }
 
+// Sha1 Calculate the sha1 hash value using the HMAC method
 func Sha1(str string, key string) string {
 	h := hmac.New(sha1.New, []byte(key))
 	h.Write([]byte(str))
 	return hex.EncodeToString(h.Sum(nil))
 }
 
+// Sha256 Calculate the sha256 hash value using the HMAC method
 func Sha256(str string, key string) string {
 	h := hmac.New(sha256.New, []byte(key))
 	h.Write([]byte(str))
