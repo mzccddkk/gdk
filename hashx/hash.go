9830991c0845518c49330e81e1c5772461945898
mzccddkk <mzccddkk@gmail.com>
2022-07-07 09:24:33 +0800

    test: adjust test case

--- hashx/hash.go
+++ hashx/hash.go
@@ -4,26 +4,34 @@ import (
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
 )
 
 // Md5 Calculate the md5 hash of a string
 func Md5(str string) string {
-	m := md5.New()
-	m.Write([]byte(str))
-	return hex.EncodeToString(m.Sum(nil))
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
 }
 
 // Sha1 Calculate the sha1 hash of a string
 func Sha1(str string) string {
-	s := sha1.New()
-	s.Write([]byte(str))
-	return hex.EncodeToString(s.Sum(nil))
+	h := sha1.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
 }
 
 // Sha256 Calculate the sha256 hash of a string
 func Sha256(str string) string {
-	s := sha256.New()
-	s.Write([]byte(str))
-	return hex.EncodeToString(s.Sum(nil))
+	h := sha256.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// Sha512 Calculate the sha512 hash of a string
+func Sha512(str string) string {
+	h := sha512.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
 }
--- hashx/hash_test.go
+++ hashx/hash_test.go
@@ -1,62 +1,107 @@
 package hashx_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/mzccddkk/gdk/hashx"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-var md5Test = map[string]string{
-	"d41d8cd98f00b204e9800998ecf8427e": "",
-	"0cc175b9c0f1b6a831c399e269772661": "a",
-	"187ef4436122d1cc2f40dc2b92f0eba0": "ab",
-	"900150983cd24fb0d6963f7d28e17f72": "abc",
-	"e2fc714c4727ee9395f324cd2e7f331f": "abcd",
-	"ab56b4d92b40713acc5af89985d4b786": "abcde",
-	"e80b5017098950fc58aad83c8c14978e": "abcdef",
+type hashTest struct {
+	hash string
+	in   string
+	out  string
 }
 
-func TestMd5(t *testing.T) {
-	for k, v := range md5Test {
-		Convey("hashx.Md5(\""+v+"\") should equal "+k, t, func() {
-			So(hashx.Md5(v), ShouldEqual, k)
-		})
-	}
+var hashTests = []hashTest{
+	{
+		"md5",
+		"Sample #1",
+		"0c5dd21f4e300bf13d57cbd1a7629822",
+	},
+	{
+		"md5",
+		"Sample #2",
+		"f7a5c31ed2326135be3cef0aee334cdf",
+	},
+	{
+		"md5",
+		"Sample #3",
+		"a03dcfd6a77d4f4997202e0ab75561ed",
+	},
+	{
+		"sha1",
+		"Sample #1",
+		"9cbd5edf990b90162d2b2568f546d7417430c695",
+	},
+	{
+		"sha1",
+		"Sample #2",
+		"0817d04b9ba6253549a57e5c90051eb73c79352a",
+	},
+	{
+		"sha1",
+		"Sample #3",
+		"86882a85ee40b2b71a40da4040df9be08a83c351",
+	},
+	{
+		"sha256",
+		"Sample #1",
+		"b733ead66b732ccb4a64e010a396642764c6a45a376ce5220e89dcbba8574f1c",
+	},
+	{
+		"sha256",
+		"Sample #2",
+		"920c288bff87f8355c385c5afea5c01abe65376a5cf8a0553124a1e27d9e640f",
+	},
+	{
+		"sha256",
+		"Sample #3",
+		"5bda2a35727fd63019d4193dc9b21771daa70f4c77748381a5c9361f9ff9e667",
+	},
+	{
+		"sha512",
+		"Sample #1",
+		"bfa7330b2402288fe01662a5f064b63302d81dd217c9b383144854942ff2cbc86b633c3de8dd9dffa270316d36140a334a2449c555524f9f76351327e3c53134",
+	},
+	{
+		"sha512",
+		"Sample #2",
+		"c450392be555deb09d257757d58e539f331d8f281e254b57fd03ddd7aea41d0f6104ca617a3ccfb8099e6e6dd9a961e7d944d066196f81ab12b41476c2f86755",
+	},
+	{
+		"sha512",
+		"Sample #3",
+		"3f9f4ffd3a0fda20f785e49cd76cd238ba8f08cc8b6d0c77fb7f7923d3a52a2fb5e9b70994886d2197c15cab08ec568d06b19fcda946d2397a90fcc4b772426e",
+	},
 }
 
-var sha1Test = map[string]string{
-	"da39a3ee5e6b4b0d3255bfef95601890afd80709": "",
-	"86f7e437faa5a7fce15d1ddcb9eaeaea377667b8": "a",
-	"da23614e02469a0d7c7bd1bdab5c9c474b1904dc": "ab",
-	"a9993e364706816aba3e25717850c26c9cd0d89d": "abc",
-	"81fe8bfe87576c3ecb22426f8e57847382917acf": "abcd",
-	"03de6c570bfe24bfc328ccd7ca46b76eadaf4334": "abcde",
-	"1f8ac10f23c5b5bc1167bda84b833e5c057a77d2": "abcdef",
-}
-
-func TestSha1(t *testing.T) {
-	for k, v := range sha1Test {
-		Convey("hashx.Sha1(\""+v+"\") should equal "+k, t, func() {
-			So(hashx.Sha1(v), ShouldEqual, k)
-		})
-	}
-}
-
-var sha256Test = map[string]string{
-	"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855": "",
-	"ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb": "a",
-	"fb8e20fc2e4c3f248c60c39bd652f3c1347298bb977b8b4d5903b85055620603": "ab",
-	"ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad": "abc",
-	"88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589": "abcd",
-	"36bbe50ed96841d10443bcb670d6554f0a34b761be67ec9c4a8ad2c0c44ca42c": "abcde",
-	"bef57ec7f53a6d40beb640a780a639c83bc29ac8a9816f1fc6c5c6dcd93c4721": "abcdef",
-}
-
-func TestSha256(t *testing.T) {
-	for k, v := range sha256Test {
-		Convey("hashx.Sha256(\""+v+"\") should equal "+k, t, func() {
-			So(hashx.Sha256(v), ShouldEqual, k)
-		})
+func TestHash(t *testing.T) {
+	for _, v := range hashTests {
+		if v.hash == "md5" {
+			i := fmt.Sprintf("hashx.Md5(\"%s\") should equal %s", v.in, v.out)
+			Convey(i, t, func() {
+				So(hashx.Md5(v.in), ShouldEqual, v.out)
+			})
+		}
+		if v.hash == "sha1" {
+			i := fmt.Sprintf("hashx.Sha1(\"%s\") should equal %s", v.in, v.out)
+			Convey(i, t, func() {
+				So(hashx.Sha1(v.in), ShouldEqual, v.out)
+			})
+		}
+		if v.hash == "sha256" {
+			i := fmt.Sprintf("hashx.Sha256(\"%s\") should equal %s", v.in, v.out)
+			Convey(i, t, func() {
+				So(hashx.Sha256(v.in), ShouldEqual, v.out)
+			})
+		}
+		if v.hash == "sha512" {
+			i := fmt.Sprintf("hashx.Sha512(\"%s\") should equal %s", v.in, v.out)
+			Convey(i, t, func() {
+				So(hashx.Sha512(v.in), ShouldEqual, v.out)
+			})
+		}
 	}
 }
