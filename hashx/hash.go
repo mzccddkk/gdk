d32739acfd3abd37e213e0cbae8140a620a481e6
mzccddkk <mzccddkk@gmail.com>
2022-07-08 15:32:57 +0800

    refactor: optimize hash function code

--- hashx/hash.go
+++ hashx/hash.go
@@ -6,32 +6,31 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"encoding/hex"
+	"hash"
 )
+
+// Hash Calculate the hash of a string
+func Hash(str string, h hash.Hash) string {
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
 
 // Md5 Calculate the md5 hash of a string
 func Md5(str string) string {
-	h := md5.New()
-	h.Write([]byte(str))
-	return hex.EncodeToString(h.Sum(nil))
+	return Hash(str, md5.New())
 }
 
 // Sha1 Calculate the sha1 hash of a string
 func Sha1(str string) string {
-	h := sha1.New()
-	h.Write([]byte(str))
-	return hex.EncodeToString(h.Sum(nil))
+	return Hash(str, sha1.New())
 }
 
 // Sha256 Calculate the sha256 hash of a string
 func Sha256(str string) string {
-	h := sha256.New()
-	h.Write([]byte(str))
-	return hex.EncodeToString(h.Sum(nil))
+	return Hash(str, sha256.New())
 }
 
 // Sha512 Calculate the sha512 hash of a string
 func Sha512(str string) string {
-	h := sha512.New()
-	h.Write([]byte(str))
-	return hex.EncodeToString(h.Sum(nil))
+	return Hash(str, sha512.New())
 }
