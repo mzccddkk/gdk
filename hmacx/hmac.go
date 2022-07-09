178abab037ecabd357beb381dc540febe2b5319d
mzccddkk <mzccddkk@gmail.com>
2022-07-09 13:05:03 +0800

    refactor: optimize hash hmac function code

--- hmacx/hmac.go
+++ hmacx/hmac.go
@@ -7,32 +7,32 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"encoding/hex"
+	"hash"
 )
+
+// Hmac Calculate the hash value using the HMAC method
+func Hmac(str string, key string, h func() hash.Hash) string {
+	hm := hmac.New(h, []byte(key))
+	hm.Write([]byte(str))
+	return hex.EncodeToString(hm.Sum(nil))
+}
 
 // Md5 Calculate the md5 hash value using the HMAC method
 func Md5(str string, key string) string {
-	h := hmac.New(md5.New, []byte(key))
-	h.Write([]byte(str))
-	return hex.EncodeToString(h.Sum(nil))
+	return Hmac(str, key, md5.New)
 }
 
 // Sha1 Calculate the sha1 hash value using the HMAC method
 func Sha1(str string, key string) string {
-	h := hmac.New(sha1.New, []byte(key))
-	h.Write([]byte(str))
-	return hex.EncodeToString(h.Sum(nil))
+	return Hmac(str, key, sha1.New)
 }
 
 // Sha256 Calculate the sha256 hash value using the HMAC method
 func Sha256(str string, key string) string {
-	h := hmac.New(sha256.New, []byte(key))
-	h.Write([]byte(str))
-	return hex.EncodeToString(h.Sum(nil))
+	return Hmac(str, key, sha256.New)
 }
 
 // Sha512 Calculate the sha512 hash value using the HMAC method
 func Sha512(str string, key string) string {
-	h := hmac.New(sha512.New, []byte(key))
-	h.Write([]byte(str))
-	return hex.EncodeToString(h.Sum(nil))
+	return Hmac(str, key, sha512.New)
 }
